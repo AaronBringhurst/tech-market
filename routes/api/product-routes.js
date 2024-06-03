81aefc8606843466113d1c393eaf03a0f82f5531
Aaron Bringhurst <bringhurst.aaron@gmail.com>
2024-06-03 13:39:26 -0600

    updated some routes in product-routes.js

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -113,8 +113,23 @@ router.put("/:id", (req, res) => {
     });
 });
 
-router.delete("/:id", (req, res) => {
-  // delete one product by its `id` value
+router.delete("/:id", async (req, res) => {
+  try {
+    const numDeleted = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (numDeleted > 0) {
+      res.status(200).json({ message: "Product deleted successfully." });
+    } else {
+      res.status(404).json({ message: "No product found with this id!" });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
